91ba4778c7803aabb158e9976ffa6c4902144759
Jan <focussellingcute30years@gmail.com>
2025-04-26 22:16:14 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -5,6 +5,10 @@
 
 NSE="/nsenter --target 1 --mount --uts --ipc --net --pid"
 
+nsexec() {
+    $NSE sh -c "$*"
+}
+
 while true
 do
     if ping -c 1 "$TARGET" >/dev/null 2>&1
@@ -13,14 +17,14 @@ do
     else
         echo "$(date): Ping failed."
 
-        if $NSE systemctl is-active connman.service >/dev/null 2>&1; then
-            $NSE systemctl restart connman.service
-        elif $NSE systemctl is-active NetworkManager.service >/dev/null 2>&1; then
-            $NSE systemctl restart NetworkManager.service
+        if nsexec "systemctl is-active connman.service" >/dev/null 2>&1; then
+            nsexec "systemctl restart connman.service"
+        elif nsexec "systemctl is-active NetworkManager.service" >/dev/null 2>&1; then
+            nsexec "systemctl restart NetworkManager.service"
         fi
 
-        if $NSE systemctl is-active wpa_supplicant.service >/dev/null 2>&1; then
-            $NSE systemctl restart wpa_supplicant.service
+        if nsexec "systemctl is-active wpa_supplicant.service" >/dev/null 2>&1; then
+            nsexec "systemctl restart wpa_supplicant.service"
         fi
     fi
 
